a8cdbb3c851b86bfaf8397657f5ca44df43add28
Akendra Chaulagain <yakendrachaulagain27@gmail.com>
2025-03-06 09:20:26 -0500

    database connetcetd

--- backend/src/database/database.js
+++ backend/src/database/database.js
@@ -1,15 +1,7 @@
 import mongoose from "mongoose";
 
 const connectDb = async () => {
-  //   try {
-  //     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-  //     //    `mongodb+srv://akendra:akendra@cluster0.zhwmt.mongodb.net/${DB_NAME}`;
-
-  //     console.log("Database Connetcetd");
-  //   } catch (error) {
-  //     console.log("Database is not connetcted", error);
-  //   }
-  // };
+  
   try {
     await mongoose.connect(`${process.env.DATABASE}/ecommerce-with-ai`);
     console.log("Database Connetcetd");
