5eb880d64066abf96e826ef077a4f08b776c4c9a
Akendra Chaulagain <yakendrachaulagain27@gmail.com>
2025-03-10 17:54:34 -0400

    done for today

--- backend/src/middleware/rateLimit.middleware.js
+++ backend/src/middleware/rateLimit.middleware.js
@@ -3,8 +3,7 @@ import rateLimit from "express-rate-limit";
 const attemptLimit = rateLimit({
   windowMs: 4 * 60 * 60,
   max: 3,
-  message:
-    "You have exceeded the 3 login attempts in 4 minutes limit! Please try again after 4 minutes",
+  message: "You have exceeded the 3 login attempts ! Please try again later.",
 });
 
 export { attemptLimit };
