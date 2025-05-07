42c6d94bb97dda18b7a9d2b1d8bad423098dd135
Akendra Chaulagain <yakendrachaulagain27@gmail.com>
2025-05-07 10:54:37 -0400

    error solved

--- frontend/hooks/axiosInstence.tsx
+++ frontend/hooks/axiosInstence.tsx
@@ -1,8 +1,10 @@
 import axios from 'axios'
 
 export const axiosInstence = axios.create({
-  baseURL: "http://localhost:5001/api/v1",
-  
+  // baseURL: "http://localhost:5001/api/v1",
+  baseURL: " https://ak-strore.onrender.com/api/v1",
+  // https://ak-strore.onrender.com/api/v1/product/all-products
+
   timeout: 3000,
   headers: {
     "Content-Type": "application/json",
